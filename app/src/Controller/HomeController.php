a3d0409fa6f7a7a9dda160cc9e0bd15ac0221185
Abu Sufian <m.abu@textmimedia.com>
2023-07-30 20:34:07 +0600

    Initial commit

---
+++ app/src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/')]
+    public function index(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
